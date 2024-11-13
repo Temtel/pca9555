e33dff91eb8d0f6c4fc027c8cff4a68ff63f2470
Temtel <temtel@gmail.com>
2024-11-12 23:38:41 -0800

    using i2c1, added help tags

--- testpca.py
+++ testpca.py
@@ -2,15 +2,19 @@ from pca9555 import PCA9555
 from machine import I2C,Pin
 from time import sleep
 
-i2c=I2C(1, scl=Pin(6), sda=Pin(7))
-devices = i2c.scan()
-print("i2c devices found:",devices)
-pca = PCA9555(i2c)
+#initialize second i2c bus on ESP32-S3 (i2c1)
+i2c1=I2C(1, scl=Pin(6), sda=Pin(7))
+devices = i2c1.scan()
+print("i2c1 devices found:",devices)
 
+#initialize the pca9555 gpio expander on the i2c1 bus
+pca = PCA9555(i2c1)
+
+#set inputs and outputs
 pca.inputPins(0)
 pca.outputPins(8)
 
-
+# poll pins for changes?
 while True:
     if pca.readPin(0) == 1:
         pca.writePin(8,1)
